a9af6d66d45bcd6a46de5933fa3e8f7bc297f104
Andrii Terytsia <a-aterytsia@microsoft.com>
2022-11-28 12:16:02 +0000

    Handling Errors

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,18 +3,29 @@ import { useState, useEffect } from "react"
 export const useFetch = (url) => {
     const [data, setData] = useState(null)
     const [isPending, setIsPending] = useState(false)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         setIsPending(true)
         const fetchData = async () => {
-            const res = await fetch(url)
-            const json = await res.json()
-            setIsPending(false)
-            setData(json)
+            try{
+                const res = await fetch(url)
+                if(!res.ok){
+                    throw new Error(res.statusText)
+                }
+                const json = await res.json()
+                setIsPending(false)
+                setData(json)
+                setError(null)
+            }catch(err){
+                setError('Could not fetch the data')
+                console.log(err.message);
+            }
+            
         }
 
         fetchData()
      }, [url])
 
-     return { data, isPending}
+     return { data, isPending, error}
 }
